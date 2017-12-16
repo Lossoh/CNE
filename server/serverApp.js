2edc1372eca1c054fd8abeb3fe7a4372451d215e
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2017-12-16 14:24:18 -0800

    Add Server ChaiHttp Testing

--- server/serverApp.js
+++ server/serverApp.js
@@ -6,6 +6,8 @@
 
 //declare all dependencies
 var express		= require('express');
+var bodyParser 	= require('body-parser');
+var favicon 	= require('serve-favicon');				//COME BACK TO THIS TO ADD
 
 //return the express object
 var serverApp = express();
@@ -13,6 +15,38 @@ var serverApp = express();
 //environment variables
 var port = process.env.PORT || 3000;
 
+//get the URL encoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+
+
+/*
+*	USE Declarations
+*
+*/
+//define our body parsers
+serverApp.use(jsonParser); // for parsing application/json
+serverApp.use(urlencodedParser); // for parsing application/x-www-form-urlencoded
+
+
+
+/*
+*	GET Declarations
+*/
+serverApp.get('/', function(req, res) {
+
+	//send back a status
+	res.sendStatus(200);
+});
+
+/*
+*	POST Declarations
+*/
+
+/*
+*	Opening Up the server
+*/
 //open the port for local development
 serverApp.listen(port,function() {
 	console.log('Express server is up and running on port ' + port);
--- test/serverApp.spec.js
+++ test/serverApp.spec.js
@@ -1,10 +1,23 @@
 //define dependencies
+var chai 		= require('chai');
+var chaiHttp 	= require('chai-http');
 var expect		= require('chai').expect;
 
-describe('CNE Server', function() {
+//define chaiHttp
+chai.use(chaiHttp);
 
-	it('Serves Up A Static Asset', function() {
-		expect(1).to.equal(1);
-	});
+//describe tests
+describe('Server Application', function() {
+
+	it('should return status code 200 on / GET', function(done) {
+		
+		chai.request('http://localhost:3000').get('/')
+		.end(function(err, res) {
+			expect(err).to.be.null;
+			expect(res).to.have.status(200);
+			done();
+		});
+
+	});	
 
 });
