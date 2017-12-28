6f313520df1ce3117b68ccaea5e2c9c9b0531c83
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2017-12-27 21:16:41 -0800

    Add: Square listTransactions Function

--- server/cli.js
+++ server/cli.js
@@ -7,9 +7,19 @@
 //declare dependcies
 var square		= require('./squareup/square');
 
-square.locations()
+square.listTransactions('14E8S7P16JQDM', 'begin_time=2017-06-01T00:00:00Z&end_time=2017-06-15T00:00:00Z')
 .then(function success(s) {
-	console.log('got this back');
+	console.log('returned:', s.length);
+	//console.log('status code:', s.status);
+	//console.log('status message:', s.statusText);
+	//console.log('status', s.headers._headers);
+	//console.log('status', s.status);
 }).catch(function error(e) {
 
 });
+
+/*square.locations()
+.then(function success(s) {
+		console.log(s);
+}).catch(function error(e) {
+});*/
--- server/squareup/square.js
+++ server/squareup/square.js
@@ -47,7 +47,8 @@ var squareup = {
 	fees: fees,
 	pages: pages,
 	webhooks: webhooks,
-	transactions: transactions,
+	chargeTransactions: chargeTransactions,
+	listTransactions: listTransactions,
 	locations: locations,
 	customers: customers,
 	checkout: checkout,
@@ -93,7 +94,107 @@ function discounts() {};
 function fees() {};
 function pages() {};
 function webhooks() {};
-function transactions() {};
+function chargeTransactions(locationId) {};
+
+/*
+*	LIST TRANSACTIONS
+*
+*	This function 
+*
+*	@params locationId (string)
+*	@params timeframe (string)
+*	@return {allTransactions}
+*/
+function listTransactions(locationId, timeframe, aCursor) {
+	//define local variables
+	var self = this;
+	var thisUrl = baseURL + 'v2/locations/' + locationId + '/transactions?' + timeframe;
+	var options = {
+		method: 'GET',
+		headers: headers
+	};
+
+	//check for cursor variables
+	if(aCursor != undefined) thisUrl += '&cursor=' + aCursor;
+
+	//return for async work
+	return new Promise(function(resolve, reject) {
+
+		//fetch the details
+		fetch(thisUrl, options)
+		.then(function success(s) {
+
+			//console.log('status headers:', s.headers._headers);
+
+			//upon success proceed
+			if(s.status == 200) {
+				
+				//retrieve buffer content
+				self._extractBuffer(s)
+				.then(function success(ss) {
+					
+					//define local variable
+					var txsArray = ss.transactions;
+
+					//check for a cursor
+					if(ss.cursor != undefined) {
+
+						//notify of the curret location
+						console.log('paging down ');
+						
+						//run more async work
+						self.listTransactions(locationId, timeframe, ss.cursor)
+						.then(function success(sss) {
+
+							//console.log('found ', sss);
+
+							//define local variables
+							var newTxsArray = sss;
+
+							//combine the arrays, by iterating through
+							newTxsArray.forEach(function(tx) {
+
+								//push the new tx onto the returnable array
+								txsArray.push(tx);
+
+							});
+
+							//
+							console.log('passing values back', txsArray.length);
+
+							//then send the values back
+							resolve(txsArray);
+						
+						}).catch(function error(e) {
+
+						});	
+
+					} else {
+
+						//let us know that we hit the bottom
+						console.log('got to the bottom', ss.transactions.length);
+
+						//pass the array of transactions back up
+						resolve(txsArray);
+
+					}
+
+				}).catch(function error(ee) {
+
+				});
+
+			} else {
+				
+				reject(s);
+			}
+
+		}).catch(function error(e) {
+
+		});
+
+	});
+
+};
 
 /*
 *	LOCATIONS
@@ -116,16 +217,23 @@ function locations() {
 		fetch(thisUrl, options)
 		.then(function success(s) {
 			
-			//retrieve buffer content
-			self._extractBuffer(s)
-			.then(function success(ss) {
+			//upon success proceed
+			if(s.status == 200) {
 				
-				//send the data back
-				resolve(ss);
+				//retrieve buffer content
+				self._extractBuffer(s)
+				.then(function success(ss) {
+					
+					//send the data back
+					resolve(ss);
 
-			}).catch(function error(ee) {
+				}).catch(function error(ee) {
 
-			});
+				});
+
+			} else {
+				reject(s);
+			}
 
 		}).catch(function error(e) {
 			console.log(e);
