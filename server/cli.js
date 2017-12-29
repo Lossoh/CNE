0ebf79420cbd6f82fb7173d38c4a4a88067ed0f5
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2017-12-28 20:40:50 -0800

    Add Timesheets Access to Sling

--- server/cli.js
+++ server/cli.js
@@ -6,4 +6,12 @@
 
 //declare dependcies
 //var square		= require('./squareup/square');
-var firebase		= require('./firebase/firebase');
+//var firebase		= require('./firebase/firebase');
+var sling			= require('./sling/sling');
+
+sling.timesheets('2017-10-22T01:00:00Z/2017-10-22T23:00:00Z')
+.then(function success(s) {
+	console.log('got this back:', s);
+}).catch(function error(e) {
+	console.log('error:', e);
+});
--- server/sling/sling.js
+++ server/sling/sling.js
@@ -12,13 +12,14 @@ var authCode 		= process.env.SLING_AUTH_CODE;
 var baseURL 		= 'https://api.sling.is/';
 var headers 		= {
 	'headers': {
-		'Accept': 'application/json'
+		'Accept': 'application/json',
 		'Authorization': authCode
 	}
 };
 
 //define module
 var sling = {
+	_extractBuffer: _extractBuffer,
 	accounts: accounts,
 	acks: acks,
 	admin: admin,
@@ -58,6 +59,30 @@ var sling = {
 	users: users
 };
 
+/*
+*	_EXTRACT BUFFER
+*
+*	Pull out the 
+*/
+function _extractBuffer(buffer) {
+	//define local variables
+	var self = this;
+
+	//return async content
+	return new Promise(function(resolve, reject) {
+
+		//access buffer
+		buffer.buffer().then(function(data) {
+
+			//pass back the json
+			resolve(JSON.parse(data.toString('utf8')));
+
+		});
+
+	});
+
+};
+
 function accounts() {};
 function acks() {};
 function admin() {};
@@ -91,10 +116,99 @@ function shift_notes() {};
 function shifts() {};
 function stripe() {};
 function tasks() {};
-function timesheets() {};
+
+/*
+*	TIMESHEETS
+*
+*	This function is used to downlooad time cards, according to the sling API.
+*	Possible Responses:
+*	200 - Success
+*	400 - The dates query string parameter was not supplied.
+*	402 - Premium required
+*	405	- Invalid permissions
+*
+*	@param op
+*	@param dates - is an interval time, i.e. 2017-10-22T01:00:00Z/2017-10-22T23:00:00Z
+*	@timesheet_id
+*	@data
+*	@returns Promise
+*
+*/
+function timesheets(dates, op, timesheet_id, data) {
+	//declare local variables
+	var self = this;
+	var url = baseURL + 'v1/timesheets?dates=' + dates;
+
+	//return async work
+	return new Promise(function(resolve, reject) {
+
+		//fetch requred data
+		fetch(url, headers)
+		.then(function success(s) {
+			
+			//extract the buffer data
+			self._extractBuffer(s)
+			.then(function success(data) {
+
+				//pass the data back
+				resolve(data);
+
+			}).catch(function error(err) {
+				reject(err);
+			});
+
+		}).catch(function error(e) {
+			reject(e);
+		});
+		
+	});
+
+};
+
 function timeszones() {};
 function uploads() {};
-function users() {};
+
+/*
+*	USERS
+*
+*	This function handles everything related to users
+*
+*	@param op
+*	@param userId
+*	@param orgId
+*	@param invitedUserId
+*	@return {returnObject}
+*/
+function users(op, userId, orgId, invitedUserId) {
+	//define local variables
+	var self = this;
+	var url = baseURL + 'v1/users';
+
+	//pass back async work
+	return new Promise(function(resolve, reject) {
+
+		//fetch requred data
+		fetch(url, headers)
+		.then(function success(s) {
+			
+			//extract the buffer data
+			self._extractBuffer(s)
+			.then(function success(data) {
+
+				//pass the data back
+				resolve(data);
+
+			}).catch(function error(err) {
+				reject(err);
+			});
+
+		}).catch(function error(e) {
+			reject(e);
+		});
+
+	});
+
+};
 
 //return the module
 module.exports = sling;
